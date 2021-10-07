db68984c6d5887c9b8bdf0479a23943c076ea417
Juan Diego <39896844+juandiegovqdev@users.noreply.github.com>
2021-10-07 14:21:35 +0200

    Create Ejercicios.py

---
+++ Ejercicios.py
@@ -0,0 +1,35 @@
+# ---------------------------------------------------------------------------
+# Escribir un programa que muestre por pantalla la cadena ¡Hola Mundo!.
+def ej1():
+    print("¡Hola Mundo!")
+# ---------------------------------------------------------------------------
+# Escribir un programa que almacene la cadena ¡Hola Mundo! en una variable y
+# luego muestre por pantalla el contenido de la variable.
+
+# ---------------------------------------------------------------------------
+# Escribir un programa que pregunte el nombre del usuario en la consola y
+# después de que el usuario lo introduzca muestre por pantalla la cadena
+# ¡Hola <nombre>!, donde <nombre> es el nombre que el usuario haya introducido.
+
+# ---------------------------------------------------------------------------
+
+# ---------------------------------------------------------------------------
+# Escribir un programa que pregunte al usuario por el número de horas
+# trabajadas y el coste por hora. Después debe mostrar por pantalla la paga
+# que le corresponde.
+
+# ---------------------------------------------------------------------------
+
+# ---------------------------------------------------------------------------
+
+# ---------------------------------------------------------------------------
+
+# ---------------------------------------------------------------------------
+
+# ---------------------------------------------------------------------------
+
+# ---------------------------------------------------------------------------
+
+# ---------------------------------------------------------------------------
+
+# ---------------------------------------------------------------------------
